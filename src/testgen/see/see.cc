ee2ed62d9dbe127b4a4b5560326bc980fad3e52c
sujitkc <sujitkc@gmail.com>
2025-10-07 07:34:22 +0530

    Added basic code to see.hh and see.cc. Compiles but doesn't do anything yet.

--- src/testgen/see/see.cc
+++ src/testgen/see/see.cc
@@ -1,1 +1,15 @@
 #include "see.hh"
+
+ResultValue::ResultValue(ResultType t) : type(t) {
+}
+
+BoolResultValue::BoolResultValue(bool v) : ResultValue(ResultType::BOOL), value(v) {
+}
+
+IntResultValue::IntResultValue(int v) : ResultValue(ResultType::INT), value(v) {
+}
+
+SymExResult::SymExResult(bool tf, map<string, unique_ptr<ResultValue> > m) : isSat(tf), model(std::move(m)) {
+}
+
+
--- src/testgen/see/see.hh
+++ src/testgen/see/see.hh
@@ -1,8 +1,44 @@
-#indef SEE_HH
+#ifndef SEE_HH
 #define SEE_HH
 
+#include<map>
+#include<memory>
+#include<string>
+
+using namespace std;
+
+enum class ResultType {
+    BOOL,
+    INT
+};
+
+class ResultValue {
+    public:
+        const ResultType type;
+        ResultValue(ResultType);
+};
+
+class BoolResultValue : public ResultValue {
+    public:
+        BoolResultValue(bool);
+        const bool value;
+};
+
+class IntResultValue : public ResultValue {
+    public:
+        IntResultValue(int);
+        const int value;
+};
+
+class SymExResult {
+    public:
+        const bool isSat;
+        const map<string, unique_ptr<ResultValue>> model; 
+        SymExResult(bool, map<string, unique_ptr<ResultValue> >);
+};
+
 class SEE {
-	public:
-		void execute();
+    public:
+        SymExResult execute();
 };
 #endif
